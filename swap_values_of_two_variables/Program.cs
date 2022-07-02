a645318f73a14e9c571789ca952ad93070c27d19
Deniz Kabaçelik <deniz.kabacelik@mctek.com.tr>
2022-07-03 00:21:55 +0300

    swap values  of two variables

---
+++ swap_values_of_two_variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace swap_values_of_two_variables
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num1, num2, temp;
+            Console.WriteLine("Enter first  number");
+            num1=Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Enter second  number");
+            num2=Convert.ToInt32(Console.ReadLine());   
+             Console.WriteLine($"number1: {num1}, number2: {num2}");
+              temp=num1;
+              num1=num2;
+              num2=temp;
+             Console.WriteLine("new number1: "+num1+" new number2: "+num2);
+        }
+    }
+}
