8a10331afbcd913220dfd9a35a6e0fc7df4f756e
Deniz Kabaçelik <deniz.kabacelik@mctek.com.tr>
2022-07-02 22:54:17 +0300

    hellowordwithdotnetconsole

---
+++ hellowordwithnetcoreconsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hellowordwithnetcoreconsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
